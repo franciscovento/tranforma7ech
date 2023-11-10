25d6eeab676cb1edaeb713bf3db8fd830ba475c0
fran <francisco.vento@outlook.com>
2023-11-10 15:47:00 -0300

    feat: alcance y software

---
+++ src/data/beneficios.ts
@@ -0,0 +1,22 @@
+const beneficios = [{
+  title: "Reducir",
+  content: 'Reducir la inconsistencia de stock físico vs lógico.'
+}, {
+  title: "Mejorar",
+  content: "Mejorar la integración de la información de las sucursales."
+}, {
+  title: "Visibilizar",
+  content: "Brindar visibilidad de información certera para la toma de decisiones.",
+}, {
+  title: "Aumentar",
+  content: "Aumentar numero de ventas reduciendo la pérdida de oportunidades por falta de stock.",
+}, {
+  title: "Centralizar",
+  content: "Centralizar información de clientes para crear programas de fidelización.",
+}, {
+  title: "Simplificar",
+  content: "Simplificar el proceso de ventas tanto online como presencial.",
+}
+]
+
+export default beneficios
